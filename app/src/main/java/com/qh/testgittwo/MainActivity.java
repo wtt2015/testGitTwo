971a980079baf2880a94561db2609713a06b2706
wtt2015 <wtt3333@126.com>
2020-03-31 15:07:25 +0800

    分支1测试提交

--- app/src/main/java/com/qh/testgittwo/MainActivity.java
+++ app/src/main/java/com/qh/testgittwo/MainActivity.java
@@ -2,6 +2,7 @@ package com.qh.testgittwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        Log.i("wtt","测试分支1");
     }
 }
