8255c4d40e0281fa7dd23cfcd4f5076bccc59198
wtt2015 <wtt3333@126.com>
2021-01-13 15:02:01 +0800

    3分支2测试提交

--- app/src/main/java/com/qh/testgittwo/MainActivity.java
+++ app/src/main/java/com/qh/testgittwo/MainActivity.java
@@ -18,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i("wtt","测试分支1");
         Log.i("wtt","2测试分支1");
         Log.i("wtt","3测试分支1");
+        Log.i("wtt","4测试分支2");
+        Log.i("wtt","5测试分支2");
     }
 }
